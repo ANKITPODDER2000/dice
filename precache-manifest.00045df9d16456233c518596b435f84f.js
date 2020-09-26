46e2f7bfd4eab503dd74b0da4e5359d0d13c8b09
Ankit Podder <ankitpodder0211@gmail.com>
2020-09-26 19:27:46 +0530

    Updates

---
+++ precache-manifest.00045df9d16456233c518596b435f84f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61722b1af48f95da349291dc2d96167d",
+    "url": "/dice/index.html"
+  },
+  {
+    "revision": "9cc1e028f17013049456",
+    "url": "/dice/static/css/main.33058ef1.chunk.css"
+  },
+  {
+    "revision": "5991bbac1c7a2b189eb5",
+    "url": "/dice/static/js/2.3bf1b04f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice/static/js/2.3bf1b04f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc1e028f17013049456",
+    "url": "/dice/static/js/main.309f2eba.chunk.js"
+  },
+  {
+    "revision": "25920a309772791770a5",
+    "url": "/dice/static/js/runtime-main.f0461a7f.js"
+  }
+]);
